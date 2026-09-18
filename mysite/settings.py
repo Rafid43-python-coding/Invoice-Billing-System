56ff7b11675d0a4dc0d40beb5e9eb98847f3d5b0
Rafid43-python-coding <rafidmohamed.4321@gmail.com>
2026-09-18 14:36:07 +0530

    Add WhiteNoise configuration for serving static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -120,7 +121,15 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+        "staticfiles" : {
+            "BACKEND": " whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+        
+    }
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
