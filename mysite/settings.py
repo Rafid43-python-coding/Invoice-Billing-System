c1e0f07b5426b5328d0b9a88a1a817d22a077320
Rafid43-python-coding <rafidmohamed.4321@gmail.com>
2026-09-18 14:49:58 +0530

    remove leading space in  STORAGES  backend string

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
         "staticfiles" : {
-            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+            "BACKEND":"whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
         
     }
